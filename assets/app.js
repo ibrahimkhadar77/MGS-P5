20a4bb85e9f91067702edccfe4645bc63b7f5149
Ibrahim Nawfalr <Ibrahim.khadar@binquraya.com>
2026-08-01 16:02:04 +0300

    Live sheet enabled

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,8 @@
 // const SHEET_ID = "1zrhLerx15lT8xp55OzhXA5M5k7eDd9aW"; // local test sheet
 const SHEET_ID = "1PHBmq5O0yvU87yrlBbJ-inuWTGJMLNYgHnfZ0IXBS7I"; // live sheet
 const CSV_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=csv`;
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwzoIp8TjW-1Ep8OqwPYDejn6Nc5mrB9pL-kM9F3jInPXRoYTV0aqOJ6ZZArYOA4WCPNg/exec";
+// const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwzoIp8TjW-1Ep8OqwPYDejn6Nc5mrB9pL-kM9F3jInPXRoYTV0aqOJ6ZZArYOA4WCPNg/exec"; // local test sheet
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzdqH02gUYfWAcH2KRUat6yLT2SmHQQV4RkK_DHmVGQXMbPF_Ekzo5d5-0NH0RwGLVFeg/exec"; // live sheet
 const DATA_SOURCE_MODE = "direct-sheet"; // apps-script | direct-sheet | auto
 const REFRESH_MS = 300 * 60 * 1000;
 
